70850912d9f56385765939e630b59daa69fd5dfb
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-02-06 11:06:31 +0100

    more setups for nuxt fullstack site

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="!text-9xl font-bold underline bg-[coral]">
+    Index
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  setup() {
+    definePageMeta({
+      ssr: true
+    })
+  }
+}
+</script>
