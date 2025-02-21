dcbed680af700246ad57e8f7ba3fc55f6a7695d2
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-02-21 23:30:12 +0100

    added parallax effect to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="w-full h-[200vh]">
-    <div
-      class="absolute bottom-0 left-0 right-0 top-0 flex items-center overflow-hidden"
-    >
+  <div class="relative">
+    <div class="absolute inset-0 flex items-center overflow-hidden">
       <img
         src="../assets/images/sky_3000px.jpg"
         alt=""
-        class="parallax-background h-full w-full object-cover"
+        class="parallax-background h-full w-full"
       />
     </div>
 
-    <div
-      class="absolute bottom-0 left-0 right-0 top-0 bg-gradient-to-b from-neutral-900 to-black/75 sm:to-black/50"
-    ></div>
+    <img
+      src="../assets/images/kund/hiss_fri.png"
+      class="object-contain w-full h-full relative"
+    />
+
+    <img
+      src="../assets/images/kund/hiss_fri.png"
+      class="object-contain w-full h-full relative"
+    />
 
     <img
       src="../assets/images/kund/hiss_fri.png"
@@ -37,11 +41,19 @@ export default {
         ".parallax-background",
       );
       parallaxElements.forEach(function (el) {
-        const rate = 0.25;
+        const rate = 0.2;
         const translateY = scrolled * rate;
         el.style.transform = `translateY(${translateY}px)`;
       });
     },
+  },
+
+  beforeMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+
+  beforeUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
   },
 };
 </script>
