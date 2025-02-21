96ee29e1d0f1b699a9e7bb9219817fa2877f47ae
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-02-21 22:50:29 +0100

    more graphical elements on home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,22 @@
 <template>
-  <div class="w-full h-screen">
+  <div class="w-full h-[200vh]">
+    <div
+      class="absolute bottom-0 left-0 right-0 top-0 flex items-center overflow-hidden"
+    >
+      <img
+        src="../assets/images/sky_3000px.jpg"
+        alt=""
+        class="parallax-background h-full w-full object-cover"
+      />
+    </div>
+
+    <div
+      class="absolute bottom-0 left-0 right-0 top-0 bg-gradient-to-b from-neutral-900 to-black/75 sm:to-black/50"
+    ></div>
+
     <img
-      src="../assets/images/sky_3000px.jpg"
-      class="object-cover w-full h-full"
+      src="../assets/images/kund/hiss_fri.png"
+      class="object-contain w-full h-full relative"
     />
   </div>
 </template>
