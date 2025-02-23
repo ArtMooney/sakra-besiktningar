ccbc110c8ce64a732a0b06883eb5bed43b446a0c
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-02-23 19:28:57 +0100

    added tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+import plugin from "tailwindcss/plugin";
+
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        gunplay: ["Gunplay", "sans-serif"],
+        libre: ["Libre Franklin", "sans-serif"],
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ addBase }) {
+      addBase({
+        body: {
+          "@apply mx-auto w-full max-w-screen-2xl bg-white font-libre font-normal text-black":
+            {},
+        },
+        "h1, .h1, h2, .h2, h3, .h3, h4, .h4, h5, .h5, h6, .h6": {
+          "@apply mb-4 mt-0 font-gunplay font-bold leading-none": {},
+        },
+
+        "h1, .h1": {
+          "@apply text-4xl md:text-5xl leading-10": {},
+        },
+
+        "h2, .h2": {
+          "@apply text-3xl md:text-4xl leading-9": {},
+        },
+
+        "h3, .h3": {
+          "@apply text-2xl md:text-3xl leading-8": {},
+        },
+
+        "h4, .h4": {
+          "@apply text-xl md:text-2xl leading-7": {},
+        },
+
+        "h5, .h5": {
+          "@apply text-lg md:text-xl leading-6": {},
+        },
+
+        "h6, .h6": {
+          "@apply text-base md:text-lg leading-5": {},
+        },
+      });
+    }),
+  ],
+  corePlugins: {
+    preflight: true,
+  },
+};
