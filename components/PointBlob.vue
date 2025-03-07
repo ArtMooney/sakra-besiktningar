a42f32972bc283950c3fc6275c427b000d418556
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-03-07 22:42:16 +0100

    broke out the point blobs

---
+++ components/PointBlob.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex text-left sm:px-0">
+    <div class="flex flex-col gap-2 bg-white p-8 sm:max-w-[95%]">
+      <h4 v-if="title" class="font-bold">{{ title }}</h4>
+
+      <ul v-if="pointLines" class="flex flex-col space-y-4 leading-normal">
+        <li v-for="line in pointLines">{{ line }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PointBlob",
+
+  props: {
+    title: {
+      type: String,
+      required: false,
+    },
+    pointLines: {
+      type: Array,
+      required: false,
+    },
+  },
+};
+</script>
--- components/Services.vue
+++ components/Services.vue
@@ -28,119 +28,60 @@ import { ChevronDoubleDownIcon } from "@heroicons/vue/24/solid";
       <h2 class="text-4xl md:text-6xl">Besiktning av</h2>
     </div>
 
-    <div
-      class="parallax-text1 relative right-0 mb-96 flex justify-end pl-4 text-left sm:px-0"
-    >
-      <div
-        class="flex flex-col gap-2 bg-white p-8 sm:col-start-2 sm:col-end-2 sm:max-w-[95%]"
-      >
-        <h4 class="font-bold">Motordrivna portar</h4>
+    <PointBlob
+      class="relative mb-96 justify-end pl-4"
+      title="Motordrivna portar"
+      :point-lines="[
+        '• Besiktningskrav enligt Boverkets föreskrift BFS 2011:12 med ändringar',
+        '• Krav på första besiktning eller att det framgår av EG-försäkran om överensstämmelse att installationen ingår i CE-märkningen',
+        '• Besiktningsintervall 24 månader',
+        '• Omfattar olika typer av motordrivna portar så som takskjutportar vikportar samt grindar',
+        '• Undantaget krav på besiktning är portar i anslutning till småbostadshus',
+      ]"
+    />
 
-        <ul class="flex flex-col space-y-4">
-          <li class="leading-normal">
-            • Besiktningskrav enligt Boverkets föreskrift BFS 2011:12 med
-            ändringar
-          </li>
-          <li class="leading-normal">
-            • Krav på första besiktning eller att det framgår av EG-försäkran om
-            överensstämmelse att installationen ingår i CE-märkningen
-          </li>
-          <li class="leading-normal">• Besiktningsintervall 24 månader</li>
-          <li class="leading-normal">
-            • Omfattar olika typer av motordrivna portar så som takskjutportar,
-            vikportar samt grindar
-          </li>
-          <li class="leading-normal">
-            • Undantaget krav på besiktning är portar i anslutning till
-            småbostadshus
-          </li>
-        </ul>
-      </div>
-    </div>
+    <PointBlob
+      class="relative mb-96 justify-start pr-4"
+      title="Lastbryggor"
+      :point-lines="[
+        '• Besiktningskrav enligt AFS 2023:11',
+        '• Besiktningsintervall 12 månader',
+        '• Första återkommande besiktning är 24 månader efter att anordningen togs i bruk första gången',
+        '• Ofta högt slitage vid regelbunden användning',
+      ]"
+    />
 
-    <div
-      class="parallax-text2 relative left-0 mb-96 flex justify-start pr-4 text-left sm:grid-cols-2 sm:px-0"
-    >
-      <div
-        class="flex flex-col gap-2 bg-white p-8 sm:col-start-1 sm:col-end-1 sm:max-w-2/3"
-      >
-        <h4 class="font-bold">Lastbryggor</h4>
-        <ul class="flex flex-col space-y-4">
-          <li class="leading-normal">• Besiktningskrav enligt AFS 2023:11</li>
-          <li class="leading-normal">• Besiktningsintervall 12 månader</li>
-          <li class="leading-normal">
-            • Första återkommande besiktning är 24 månader efter att anordningen
-            togs i bruk första gången
-          </li>
-          <li class="leading-normal">
-            • Ofta högt slitage vid regelbunden användning
-          </li>
-        </ul>
-      </div>
-    </div>
+    <PointBlob
+      class="relative mb-96 justify-end pl-4"
+      title="Mobila arbetsplattformar"
+      :point-lines="[
+        '• Besiktningskrav enligt AFS 2023:11',
+        '• Besiktningsintervall: 12 månader',
+        '• Första återkommande besiktning är 12 månader efter att maskinen togs i bruk för första gången',
+      ]"
+    />
 
-    <div
-      class="parallax-text3 relative right-0 z-1 mb-96 flex justify-end pl-4 text-left sm:grid-cols-2 sm:px-0"
-    >
-      <div class="flex flex-col gap-2 bg-white p-8 sm:col-start-2 sm:col-end-2">
-        <h4 class="font-bold">Mobila arbetsplattformar</h4>
-        <ul class="flex flex-col space-y-4">
-          <li class="leading-normal">• Besiktningskrav enligt AFS 2023:11</li>
-          <li class="leading-normal">• Besiktningsintervall: 12 månader</li>
-          <li class="leading-normal">
-            • Första återkommande besiktning är 12 månader efter att maskinen
-            togs i bruk för första gången
-          </li>
-        </ul>
-      </div>
-    </div>
+    <PointBlob
+      class="relative mb-96 justify-start pr-4"
+      title="Plocktruckar"
+      :point-lines="[
+        '• Besiktningskrav enligt AFS 2023:11',
+        '• Besiktningsintervall 12 månader',
+        '• Första återkommande besiktning 12 månader efter att maskinen togs i bruk för första gången',
+        '• Undantagna för besiktningskrav är plocktruckar vars lyfthöjd understiger 1,2 meter. Krav på periodisk kontroll av kunnig person kvarstår',
+      ]"
+    />
 
-    <div
-      class="parallax-text1 relative left-0 mb-96 flex justify-start pr-4 text-left sm:grid-cols-2 sm:px-0"
-    >
-      <div
-        class="flex flex-col gap-2 bg-white p-8 sm:col-start-1 sm:col-end-1 sm:max-w-2/3"
-      >
-        <h4 class="font-bold">Plocktruckar</h4>
-        <ul class="flex flex-col space-y-4">
-          <li class="leading-normal">• Besiktningskrav enligt AFS 2023:11</li>
-          <li class="leading-normal">• Besiktningsintervall 12 månader</li>
-          <li class="leading-normal">
-            • Första återkommande besiktning 12 månader efter att maskinen togs
-            i bruk för första gången
-          </li>
-          <li class="leading-normal">
-            • Undantagna för besiktningskrav är plocktruckar vars lyfthöjd
-            understiger 1,2 meter. Krav på periodisk kontroll av kunnig person
-            kvarstår
-          </li>
-        </ul>
-      </div>
-    </div>
-
-    <div
-      class="parallax-text2 relative right-0 z-1 mb-96 flex justify-end pl-4 text-left sm:grid-cols-2 sm:px-0"
-    >
-      <div
-        class="flex flex-col gap-2 bg-white p-8 sm:col-start-2 sm:col-end-2 sm:max-w-2/3"
-      >
-        <h4 class="font-bold">Fordonslyftar</h4>
-        <ul class="flex flex-col space-y-4">
-          <li class="leading-normal">• Besiktningskrav enligt AFS 2023:11</li>
-          <li class="leading-normal">• Besiktningsintervall 12 mån</li>
-          <li class="leading-normal">
-            • Vår erfarenhet är att fordonslyftar har fler allvarliga brister än
-            andra typer av anordningar. Utför besiktning för att uppfylla
-            lagkraven samt bidra till en säkrare arbetsplats
-          </li>
-          <li class="leading-normal">
-            • Undantagna kravet på besiktning är fordonslyftar vilka inte är
-            avsedda för arbete där arbetstagaren befinner sig under fordonet.
-            Krav på periodisk kontroll av kunnig person kvarstår
-          </li>
-        </ul>
-      </div>
-    </div>
+    <PointBlob
+      class="relative mb-96 justify-end pl-4"
+      title="Fordonslyftar"
+      :point-lines="[
+        '• Besiktningskrav enligt AFS 2023:11',
+        '• Besiktningsintervall 12 mån',
+        '• Vår erfarenhet är att fordonslyftar har fler allvarliga brister än andra typer av anordningar. Utför besiktning för att uppfylla lagkraven samt bidra till en säkrare arbetsplats',
+        '• Undantagna kravet på besiktning är fordonslyftar vilka inte är avsedda för arbete där arbetstagaren befinner sig under fordonet. Krav på periodisk kontroll av kunnig person kvarstår',
+      ]"
+    />
   </div>
 
   <div class="relative my-8 w-full border-neutral-200 px-4">
