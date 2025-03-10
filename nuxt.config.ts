9a188cd9503f19d6b5d918363b49a7be6e6367f2
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-03-10 22:27:12 +0100

    started configuring env keys for nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,18 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ['/assets/css/main.css'],
+  css: ["/assets/css/main.css"],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
-})
+  runtimeConfig: {
+    MAILGUN_API_KEY: process.env.MAILGUN_API_KEY,
+
+    public: {
+      USERNAME: process.env.NUXT_PUBLIC_USERNAME,
+      USERPASS: process.env.NUXT_PUBLIC_USERPASS,
+    },
+  },
+});
