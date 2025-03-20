5ec9f67412377678516a1decfc65b2be6b113a48
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-03-20 09:08:32 +0100

    content and styling changes on home page

--- components/Home.vue
+++ components/Home.vue
@@ -3,7 +3,7 @@ import { ChevronDoubleDownIcon } from "@heroicons/vue/24/solid";
 </script>
 
 <template>
-  <div class="relative overflow-hidden pb-96">
+  <div class="relative overflow-hidden pb-64">
     <div class="absolute inset-0 flex items-center overflow-hidden">
       <img
         src="../assets/images/sky_3000px.jpg"
@@ -32,9 +32,7 @@ import { ChevronDoubleDownIcon } from "@heroicons/vue/24/solid";
       class="relative h-full w-full min-w-[200vw] -translate-x-20 object-cover sm:min-w-0 sm:translate-x-0"
     />
 
-    <div
-      class="relative right-0 mt-20 flex justify-end pl-4 text-left sm:mt-12 sm:px-0"
-    >
+    <div class="relative right-0 mt-20 flex justify-end pl-4 text-left sm:px-0">
       <div
         class="flex flex-col gap-2 bg-white p-8 sm:col-start-2 sm:col-end-2 sm:max-w-2/3"
       >
@@ -50,11 +48,12 @@ import { ChevronDoubleDownIcon } from "@heroicons/vue/24/solid";
 
     <img
       src="../assets/images/travers.png"
-      class="relative mt-16 mb-32 h-auto w-full object-cover sm:mt-20 md:mt-12 md:mb-96"
+      alt=""
+      class="relative h-auto w-full object-cover"
     />
 
     <div
-      class="relative left-0 -mt-24 flex justify-start pr-4 text-left sm:grid-cols-2 sm:px-0 md:-mt-96 lg:-mt-[32rem]"
+      class="relative left-0 flex justify-start pr-4 text-left sm:grid-cols-2 sm:px-0"
     >
       <div
         class="flex flex-col gap-2 bg-white p-8 sm:col-start-1 sm:col-end-1 sm:max-w-2/3"
@@ -68,7 +67,7 @@ import { ChevronDoubleDownIcon } from "@heroicons/vue/24/solid";
     </div>
 
     <div
-      class="relative right-0 z-1 mt-96 flex justify-end pl-4 text-left sm:grid-cols-2 sm:px-0"
+      class="relative right-0 mt-20 flex justify-end pl-4 text-left sm:grid-cols-2 sm:px-0"
     >
       <div
         class="flex flex-col gap-2 bg-white p-8 sm:col-start-2 sm:col-end-2 sm:max-w-2/3"
@@ -76,16 +75,10 @@ import { ChevronDoubleDownIcon } from "@heroicons/vue/24/solid";
         <h4 class="font-bold">Personlig Service</h4>
         <p>
           Vi erbjuder personlig kontakt och flexibla lösningar anpassade till
-          era behov. Vi strävar efter att erbjuda tjänster som matchar era
-          önskemål för att skapa en smidig och professionell process.
+          era behov.
         </p>
       </div>
     </div>
-
-    <img
-      src="../assets/images/green-lift.png"
-      class="absolute bottom-0 -left-30 h-auto w-[34rem] min-w-[34rem] sm:left-0"
-    />
 
     <ChevronDoubleDownIcon
       class="fixed bottom-4 left-4 h-12 w-12 text-white transition-opacity duration-500 ease-in-out"
