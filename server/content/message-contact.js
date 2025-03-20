8158d854cf4bb560787a2913f24a044beea8469b
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-03-20 15:56:59 +0100

    changed the form answer

--- server/content/message-contact.js
+++ server/content/message-contact.js
@@ -1,16 +1,8 @@
 export async function messageContact() {
-  return `Hej,
-
-
-Kul att ni vill kontakta oss på Säkra besiktningar! Vi har fått ert email och återkommer snart till er.
-
-
-
-Vänliga hälsningar,
-
-
-
-/ Markus Lindälv
-www.sakrabesiktningar.se
+  return `Tack för att du kontaktat oss!
+ 
+Vi har mottagit ditt formulär och kommer att behandla det så snabbt vi kan. Om du önskar komma i kontakt med oss direkt, vänligen ring oss på 0760 – 05 75 15. Vi ser fram emot att hjälpa dig och återkommer till dig inom kort.
+ 
+Tack för att du valde Säkra besiktningar.
 `;
 }
