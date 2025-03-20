8d960684f8b5e7f172d8c59b9da1289276e3cf3b
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-03-20 21:00:22 +0100

    added favicon set

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,4 +20,25 @@ export default defineNuxtConfig({
       userPass: process.env.NUXT_PUBLIC_USERPASS,
     },
   },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
+        { rel: "manifest", href: "/site.webmanifest" },
+      ],
+    },
+  },
 });
