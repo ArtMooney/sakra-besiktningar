180d945e611a3cb65d1830da40c50d2e44f9d2b9
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-04-11 15:52:44 +0200

    logging test

--- server/api/contact.js
+++ server/api/contact.js
@@ -6,7 +6,11 @@ export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
   const headers = getHeaders(event);
 
-  return { status: "test ok", data: config };
+  return {
+    status: "test ok",
+    data: config,
+    pass: [env.NUXT_MAILGUN_API_KEY, env.MAILGUN_API_KEY],
+  };
 
   if (!(await checkLogin(headers, config.userName, config.userPass))) {
     throw createError({
