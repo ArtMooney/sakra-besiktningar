a89ed805335e1b4215174cf19f7f301392fde1bb
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2026-02-04 09:27:46 +0100

    use seo from cms

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,19 +1,5 @@
 <script setup>
-useSeoMeta({
-  title:
-    "Säkra besiktningar - Ackrediterad besiktning av hissar och lyftanordningar",
-  description:
-    "Vi utför ackrediterad och oberoende besiktning av hissar, portar och lyftanordningar. Lokal partner med personlig service i Kungsbacka och hela Sverige.",
-  ogTitle: "Säkra besiktningar - Ackrediterad besiktning",
-  ogDescription:
-    "Vi utför ackrediterad och oberoende besiktning av hissar, portar och lyftanordningar. Lokal partner med personlig service.",
-  ogImage: "/images/og-image-home.jpg",
-  twitterCard: "summary_large_image",
-  twitterTitle: "Säkra besiktningar - Ackrediterad besiktning",
-  twitterDescription:
-    "Vi utför ackrediterad och oberoende besiktning av hissar, portar och lyftanordningar. Lokal partner med personlig service.",
-  twitterImage: "/images/twitter-image-home.jpg",
-});
+useCmsSeo("SEO page - Index");
 
 definePageMeta({
   ssr: true,
